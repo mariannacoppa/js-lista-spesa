a55f092232ff1e2ea7123991d430dab97317a90f
mariannacoppa <densimetra@gmail.com>
2024-07-02 13:39:18 +0200

    [UPD] get element from html

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 // creo array contentente la lista della spesa
 const ingredients = ['guanciale', 'peperoni', 'pasta', 'olive', 'controfiletto', 'quaglie', 'insalata', 'pollo', 'gamberi', 'pomodori', 'melanzane'];
+// recupero l'elemento che deve contenere la lista
+const lista_spesa = document.getElementById('lista_spesa');
+// dichiaro e inizializzo la variabile contatore
+let i = 0;
