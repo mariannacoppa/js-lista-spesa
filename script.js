b6931c5677570566320d10ec837f4931ed8bc72a
mariannacoppa <densimetra@gmail.com>
2024-07-02 13:34:52 +0200

    [UPD] create html tag and js array

---
+++ script.js
@@ -0,0 +1,2 @@
+// creo array contentente la lista della spesa
+const ingredients = ['guanciale', 'peperoni', 'pasta', 'olive', 'controfiletto', 'quaglie', 'insalata', 'pollo', 'gamberi', 'pomodori', 'melanzane'];
